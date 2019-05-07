94849446c62c4cc4ad511d023754c4d3c71766e2
Ênio Filipe <filipeenio1234@gmail.com>
2019-05-06 23:21:24 -0300

    Login Authenticated 0.1

--- rotas/dashboard.js
+++ rotas/dashboard.js
@@ -5,7 +5,6 @@ const {ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
 router.get('/home', ensureAuthenticated, (req, res) => {
     let query = "SELECT * FROM `nomes` ORDER BY id ASC"; // query database to get all the players
-    console.log(req.user.email);
     // execute query
     con.query(query, (err, result) => {
         
